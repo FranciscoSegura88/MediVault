6656cda3fc161d796fd846daf0ab983ace7fd203
FranciscoSegura88 <125767908+FranciscoSegura88@users.noreply.github.com>
2024-05-02 16:13:32 -0600

    pura mierda culeros

---
+++ forms/form_login.py
@@ -0,0 +1,18 @@
+import tkinter as tk
+from tkinter import ttk, messagebox
+from tkinter.font import BOLD
+#Dios, porfavor dame la paciencia y el conocimiento del porque putas madres esta dando error este import si el hijo de puta directorio
+#esta en la misma maldita carpeta y tiene el puto nombre del archivo.py, porfavor Dios ayudame 
+import util.generic as utl
+from forms.form_master import MasterPanel
+
+class App:
+    def __init__(self):
+        self.ventana = tk.Tk()
+        self.ventana.title("Inicio de sesion")
+        self.ventana.geometry("800x500")
+        self.ventana.config(bg = "#fcfcfc")
+        self.ventana.resizable(width=0, height=0)
+        utl.centrar_ventana(self.ventana, 800, 500)
+
+        self.ventana.mainloop()
