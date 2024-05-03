7236ee2b56310e414e4e5a939c186273d7ceae1a
FranciscoSegura88 <125767908+FranciscoSegura88@users.noreply.github.com>
2024-05-02 19:27:36 -0600

    pura pinche gente de ciencias computacionales

--- forms/form_login.py
+++ forms/form_login.py
@@ -5,32 +5,68 @@ import util.generic as utl
 from forms.form_master import MasterPanel
 
 class App:
+
+    def verificar(self):
+        usu = self.usuario.get()
+        password = self.password.get()
+        if(usu == "root" and password == "1234"):
+            self.ventana.destroy()
+            MasterPanel()
+        else:
+            messagebox.showError(message="La contrase;a no es correcta", title = "Error")
+
     def __init__(self):
         self.ventana = tk.Tk()
-        self.ventana.title("Inicio de sesion")
+        self.ventana.title("Inicio de sesión")
         self.ventana.geometry("500x600")
-        self.ventana.config(bg = "#fcfcfc")
+        self.ventana.config(bg="#fcfcfc")
         self.ventana.resizable(width=0, height=0)
         utl.centrar_ventana(self.ventana, 500, 600)
-        
+
         logo = utl.leer_imagen("./images/logo.png", (200, 200))
 
-        #TOP
-        frame_logo = tk.Frame(self.ventana, bd = 0, height= 200, relief = tk.SOLID, padx = 5, pady = 2, bg = "#ffd700")
-        frame_logo.pack(side = "top", expand = tk.NO, fill=tk.BOTH)
+        # Frame del logo
+        frame_logo = tk.Frame(self.ventana, bd=0, height=200, relief=tk.SOLID, padx=5, pady=2, bg="#ffd700")
+        frame_logo.pack(side="top", expand=tk.NO, fill=tk.BOTH)
         label = tk.Label(frame_logo, image=logo, bg="#ffd700")
-        label.place(x = 0, y = 0, relwidth = 1, relheight = 1)
+        label.pack(fill=tk.BOTH, expand=True)
 
-        #Crea frame a la derecha
-        frame_form = tk.Frame(self.ventana, bd = 0, relief = tk.SOLID, bg = "#fcfcfc")
-        frame_form.pack(side = "bottom", expand = tk.YES, fill = tk.BOTH)
+        # Frame del formulario
+        frame_form = tk.Frame(self.ventana, bd=0, relief=tk.SOLID, bg="#fcfcfc")
+        frame_form.pack(side="bottom", expand=tk.YES, fill=tk.BOTH)
 
-        frame_form_bottom = tk.Frame(frame_form, height = 300, bd = 0, relief = tk.SOLID, bg = "#fcfcfc")
-        frame_form_bottom.pack(side = "top", fill = tk.BOTH)
-        title = tk.Label(frame_form_bottom, text = "Inicio de Sesion", font=("Arial", 30), bg="#fcfcfc", pady=10,)
-        title.pack(expand = tk.YES, fill = tk.BOTH)
+        # Frame del texto "inicio de sesión"
+        frame_form_text = tk.Frame(frame_form, bd=0, relief=tk.SOLID, bg="#fcfcfc")
+        frame_form_text.pack(side="top", fill=tk.BOTH)
+        title = tk.Label(frame_form_text, text="Inicio de Sesión", font=("Arial", 30), bg="#fcfcfc", pady=10)
+        title.pack(fill=tk.BOTH, expand=True)
 
+        # Frame del entry de usuario
+        frame_usuario = tk.Frame(frame_form, bd=0, relief=tk.SOLID, bg="#fcfcfc")
+        frame_usuario.pack(fill=tk.X, padx=20, pady=5)
+
+        etiqueta_usuario = tk.Label(frame_usuario, text="Usuario", font=("Arial", 14), bg="#fcfcfc")
+        etiqueta_usuario.pack(side="left", padx=(20,10), anchor="center", pady=40)
+
+        self.usuario = ttk.Entry(frame_usuario, font=("Arial", 14))
+        self.usuario.pack(fill=tk.X, padx=(0,20), pady=40)
+
+        # Frame del entry de la contraseña
+        frame_password = tk.Frame(frame_form, bd=0, relief=tk.SOLID, bg="#fcfcfc")
+        frame_password.pack(fill=tk.X, padx=20, pady=5)
+
+        etiqueta_password = tk.Label(frame_password, text="Contraseña", font=("Arial", 14), bg="#fcfcfc")
+        etiqueta_password.pack(side="left", padx=(20,10), anchor="center", pady=4)
+
+        self.password = ttk.Entry(frame_password, font=("Arial", 14))
+        self.password.config(show="*")
+        self.password.pack(fill=tk.X, padx=(0,20))
         
+        #Boton inicio de Sesion
+        inicio = tk.Button(frame_form, text = "Iniciar sesion", font = ("Arial", 18, BOLD), bg = "#008000", command = self.verificar)
+        inicio.pack(padx= 5, pady= 25)
+        #TODO: No esta funcionando darle enter
+        inicio.bind("<Return>", (lambda event: self.verificar()))
 
         self.ventana.mainloop()
 
